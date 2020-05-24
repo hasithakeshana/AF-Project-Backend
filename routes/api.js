8eb22829461963673c6d0190f8d813f85c6f7d8c
hasitha <hasithakeshana9900@gmail.com>
2020-05-24 18:30:27 +0530

    copy

--- routes/api.js
+++ routes/api.js
@@ -683,4 +683,59 @@ router.post("/items", upload.array('productImage', 4) , (req, res) => {   // add
       });
 });
 
+
+router.post('/signupManager',function(req,res,next){
+  
+  console.log(req.body);
+
+  User.findOne({ email: req.body.email}). then(manager =>{
+    if(manager) {
+      res.status(400).send({email:"User with email already exists"} );
+    } else {
+
+      const managerData = {
+        firstName : req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        role: "Manager",
+      }         
+      
+      User.create(managerData).then(function(manager){
+
+      res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Methods" , "POST, GET, OPTIONS");
+        
+
+      res.setHeader('Content-Type', 'application/json');
+      //res.status(200).send(JSON.stringify({success:"registerd successfully" , code : 'reg', user : user} ));
+      
+      //new verification token is created for the new user
+            var token = new Token({ _userId: manager._id, token: crypto.randomBytes(16).toString('hex') });
+            
+            //save the verification token
+            token.save(function (err) {
+              if (err) {
+                return res.status(500).send({ msg: err.message }); 
+              }
+
+              //send the email
+              var transporter = nodemailer.createTransport({ service: 'gmail', port: 25, secure: false , auth: { user: 'donotrep2ly921@gmail.com', pass: '0711920012' }, tls: { rejectUnauthorized: false } });                                          
+              var mailOptions = { from: 'donotrep2ly921@gmail.com', 
+                                  to: manager.email, 
+                                  subject: 'Manager Account Verification', 
+                                  text: 'Hello, \n\n' + 
+                                  'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/api\/confirmation\/' + token.token + '\/' +  manager.email + '\n' }; 
+              transporter.sendMail(mailOptions, function (err) {
+                if (err) { return res.status(500).send({ msg: err.message }); }
+                res.status(200).send('A verification email has been sent to ' + user.email + '.');
+              });
+            });
+    }).catch(next);
+  }
+});
+
+});
+
 module.exports = router;
